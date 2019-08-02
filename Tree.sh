e00e5a19a566e152680c1e87e5d90f84dedfb26d
edkalrio <edkalrio@lunaticgeek.com>
2019-08-02 14:13:38 +0200

    add lights function

--- Tree.sh
+++ Tree.sh
@@ -3,12 +3,23 @@
 cols=$(tput cols)
 rows=$(tput lines)
 center=$(( cols/2 ))
-
-tput clear
+max_H=$(( (rows - 4)/4 - 1 ))
+max_W=$(( (cols - 15)/8 - 1 ))
+tree_size=$(( max_H < max_W ? max_H : max_W ))
 
 addchar() {
 	tput cup "$1" $(( center + "$2" ))
 	echo "$3"
+}
+
+tree_area() {
+	for (( i = 0; i < tree_size; i++ )); do
+		for (( j = 0; j < 4; j++ )); do
+			k=$(( 4*i + j ))
+			w[k]=$(( 8*i + 4*j + 1 ))
+			area=$(( ${w[@]/%/+}0 ))
+		done
+	done
 }
 
 draw_star() {
@@ -34,7 +45,7 @@ draw_star() {
 }
 
 grow_tree() {
-	for (( i = 0; i < 9; i++ )); do
+	for (( i = 0; i < tree_size; i++ )); do
 		for (( j = 0; j < 4; j++ )); do
 			addchar $(( 4*i + j + 4 )) $((   4*i + 2*j + 1 )) "\\"
 			addchar $(( 4*i + j + 4 )) $(( - 4*i - 2*j - 1 )) "/"
@@ -42,5 +53,26 @@ grow_tree() {
 	done
 }
 
+lights() {
+	for (( i = 0; i < area/10; i++ )); do
+		tree_H=$(( ${#w[*]} ))
+		random_pre_y=$(( RANDOM % tree_H ))
+		random_pre_x=$(( RANDOM % cols ))
+		if [[ $random_pre_x -lt ${w[random_pre_y]} ]]; then
+			random_y=$(( random_pre_y + 4 ))
+			random_x=$(( (random_pre_x - 1) / 2 ))
+			if [[ $((RANDOM % 2)) -eq 1 ]]; then
+				random_x=$(( - random_x ))			
+			fi	
+			addchar $random_y $random_x "o"
+		fi
+	done
+}
+
+tput clear
+
 grow_tree
 draw_star
+
+tree_area
+lights
